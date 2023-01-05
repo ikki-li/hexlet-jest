b8113389dc390c3d7513ce75c4da43b4700ba817
Alexandra Yakshina <alexandra.yakshina@gmail.com>
2023-01-05 15:35:37 +0300

    test stack function

---
+++ __tests__/stack.test.js
@@ -0,0 +1,24 @@
+import makeStack from '../src/stack.js';
+
+test("stack's main flow", () => {
+	const stack = makeStack();
+	stack.push('one');
+	stack.push('two');
+	expect(stack.pop()).toEqual('two');
+	expect(stack.pop()).toEqual('one');
+});
+
+test('isEmpty', () => {
+	const stack = makeStack();
+	expect(stack.isEmpty()).toBe(true);
+	stack.push('two');
+	expect(stack.isEmpty)).toBe(false);
+	stack.pop();
+	expect(stack.isEmpty()).toBe(true);
+});
+
+test('pop in empty stack', () => {
+	const stack = maleStack();
+	expect(() => stack.pop()).toThrow();
+});
+
